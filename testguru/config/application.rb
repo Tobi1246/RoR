561aefcb4d4ed1b7aee7fd34d8c18f71cdc392e4
Tobi1246 <d.morox@ya.ru>
2022-12-02 14:01:25 +0300

    Set locale and lang

--- testguru/config/application.rb
+++ testguru/config/application.rb
@@ -11,12 +11,26 @@ module Testguru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # config/initializers/locale.rb
+
+    # Where the I18n library should search for translation files
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :pt]
+
+    # Set default locale to something other than :en
+    I18n.default_locale = :pt
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Minsk'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
