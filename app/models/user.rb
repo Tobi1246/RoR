c166be81452a587194fc93c51b0fa807e89adc40
Tobi1246 <d.morox@ya.ru>
2023-02-01 17:33:59 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
-	class User < ApplicationRecord
+class User < ApplicationRecord
   def levels(lvl)
-	 self.joins("INNER JOIN test_users ON test_users.user_id = users.id
-	 						 INNER JOIN tests ON tests.id = test_users.test_id
-							 WHERE test_id = user_id")
+	 TestUser.joins("INNER JOIN users ON test_users.user_id = users.id
+	 						 INNER JOIN tests ON test_users.test_id = tests.id")
 	   .where(tests: {level: lvl})
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,27 +12,30 @@ Test.destroy_all
 Category.destroy_all
 User.destroy_all
 
-user = User.create!([{firstname: "Lion", lastname: "Di", age: 18},
+users = User.create!([{firstname: "Lion", lastname: "Di", age: 18},
                      {firstname: "Tod", lastname: "Darck", age: 48},
                      {firstname: "Joi", lastname: "Brain", age: 27}])
-category = Category.create!([{title: "Ruby"},
+categorys = Category.create!([{title: "Ruby"},
              {title: "HTML"},
              {title: "SQL"}])
-test = Test.create!([{title:"Ruby", level: 1, author_id: user.at(0).id, category_id: category.at(0).id},
-                     {title:"HTML", level: 2, author_id: user.at(1).id, category_id: category.at(1).id},
-                     {title:"SQL", level: 2, author_id: user.at(2).id, category_id: category.at(2).id}])
-question = Question.create!([{title:"What is object in Ruby", test_id: test.at(0).id},
-                             {title:"What is teg <li>", test_id: test.at(1).id},
-                             {title:"How to multi merge tables", test_id: test.at(2).id}])
-Answer.create!(question_id: question.at(0).id,
+tests = Test.create!([{title:"Ruby", level: 1, author_id: users.at(0).id, category_id: categorys.at(0).id},
+                     {title:"HTML", level: 2, author_id: users.at(1).id, category_id: categorys.at(1).id},
+                     {title:"SQL", level: 2, author_id: users.at(2).id, category_id: categorys.at(2).id}])
+questions = Question.create!([{title:"What is object in Ruby", test_id: tests.at(0).id},
+                             {title:"What is teg <li>", test_id: tests.at(1).id},
+                             {title:"How to multi merge tables", test_id: tests.at(2).id}])
+Answer.create!(question_id: questions.at(0).id,
               body: "Object is the default root of all Ruby objects. 
               Object inherits from BasicObject which allows creating alternate object hierarchies.
               Methods on Object are available to all classes unless explicitly overridden.")
-Answer.create!(correct: false, question_id: question.at(0).id, body: "Class")
-Answer.create!(correct: false, question_id: question.at(0).id, body: "All except Class")
-Answer.create!(question_id: question.at(1).id, body: "List item")
-Answer.create!(correct: false, question_id: question.at(1).id, body: "link")
-Answer.create!(correct: false, question_id: question.at(1).id, body: "Create sort list")
-Answer.create!(question_id: question.at(2).id, body: "Usege JOIN  after constructions JOIN ON")
-Answer.create!(correct: false, question_id: question.at(2).id, body: "One usege JOIN and listing tables")
-Answer.create!(correct: false, question_id: question.at(2).id, body: "INNER JOIN")
+Answer.create!(correct: false, question_id: questions.at(0).id, body: "Class")
+Answer.create!(correct: false, question_id: questions.at(0).id, body: "All except Class")
+Answer.create!(question_id: questions.at(1).id, body: "List item")
+Answer.create!(correct: false, question_id: questions.at(1).id, body: "link")
+Answer.create!(correct: false, question_id: questions.at(1).id, body: "Create sort list")
+Answer.create!(question_id: questions.at(2).id, body: "Usege JOIN  after constructions JOIN ON")
+Answer.create!(correct: false, question_id: questions.at(2).id, body: "One usege JOIN and listing tables")
+Answer.create!(correct: false, question_id: questions.at(2).id, body: "INNER JOIN")
+TestUser.create!([{test_id: tests.at(0).id, user_id: users.at(0).id},
+                  {test_id: tests.at(1).id, user_id: users.at(1).id},
+                  {test_id: tests.at(2).id, user_id: users.at(2).id}])
