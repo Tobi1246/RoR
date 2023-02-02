40ed0811c80d4501a7d1be35426cb87c20dec76f
Tobi1246 <d.morox@ya.ru>
2023-02-02 17:52:27 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-	def levels(lvl)
-	 TestUser.joins("INNER JOIN users ON test_users.user_id = users.id
-									 INNER JOIN tests ON test_users.test_id = tests.id")
-						.where(users: {id: self.id},
-									tests: {level: lvl})
-	end
+  def levels(lvl)
+   TestUser.joins("INNER JOIN users ON test_users.user_id = users.id
+                   INNER JOIN tests ON test_users.test_id = tests.id")
+            .where(users: {id: self.id},
+                  tests: {level: lvl})
+  end
 end
