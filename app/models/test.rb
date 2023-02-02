dcc4c1cc39137ebaa7986f13daa37effa78822c7
Tobi1246 <d.morox@ya.ru>
2023-02-03 02:53:47 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
   def self.desc_ttitle_and_category(category)
     joins("INNER JOIN categories ON tests.category_id = categories.id")
       .where(categories: {title: category})
-      .order('test.title DESC')
+      .order(title: :desc)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   def levels(lvl)
-   TestUser.joins("INNER JOIN users ON test_users.user_id = users.id
-                   INNER JOIN tests ON test_users.test_id = tests.id")
-              .where(users: {id: self.id},
-                  tests: {level: lvl})
+   Test.select("tests.level, tests.title, users.firstname")
+         .joins("JOIN test_users ON test_users.user_id = users.id
+                 JOIN users ON test_users.test_id = tests.id")
+         .where(users: {id: self.id},
+                tests: {level: lvl})
   end
 end
