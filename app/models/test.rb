fcb4a9b9c35a9fedbd315e8bd4d2eae1fe9c6bd0
Tobi1246 <d.morox@ya.ru>
2023-02-02 17:55:35 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
-	def self.desc_ttitle_and_category(category)
-		joins("INNER JOIN categories ON tests.category_id = categories.id")
-		  .where(categories: {title: category})
-		  .order('test.title DESC')
-	end
+  def self.desc_ttitle_and_category(category)
+    joins("INNER JOIN categories ON tests.category_id = categories.id")
+      .where(categories: {title: category})
+      .order('test.title DESC')
+  end
 end
