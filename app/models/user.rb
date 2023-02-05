c797c91eef7694cb0d48dead2bb0d33918b3c8ad
Tobi1246 <d.morox@ya.ru>
2023-02-05 12:26:51 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   def levels(lvl)
    Test.joins("JOIN test_users ON test_users.test_id = tests.id")
-         .where(tests: {level: lvl})
+         .where(test_users: {user_id: self.id},
+                tests: {level: lvl})
   end
 end
