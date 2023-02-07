be84a4e1ae11a3aa0e13d1fea7b10b1b29a94462
Tobi1246 <d.morox@ya.ru>
2023-02-07 13:43:44 +0300

    assoсiations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+
+  has_many :test_users
+  has_many :users, through: :test_users
+  has_many :questions
+  has_one :category
+
   def self.desc_ttitle_and_category(category_title )
     joins("INNER JOIN categories ON tests.category_id = categories.id")
       .where(categories: {title: category_title})
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :test_users
+  has_many :tests, through: :test_users
+  
   def levels(lvl)
     Test.joins("JOIN test_users ON test_users.test_id = tests.id")
           .where(test_users: {user_id: self.id},
