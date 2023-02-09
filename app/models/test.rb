dd44a8dc0405d7438bbca893dcadd295217f6326
Tobi1246 <d.morox@ya.ru>
2023-02-09 15:45:46 +0300

    fix0.2

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
 
+  belongs_to :author, class_name: "User"
   belongs_to :category
-
-  has_many :test_users, dependent: :destroy
+ 
+  has_many :test_users
   has_many :users, through: :test_users, dependent: :destroy
-  has_many :users, through: :test_users, source: :author, dependent: :destroy
   has_many :questions, dependent: :destroy
-
+ 
   def self.desc_ttitle_and_category(category_title )
     joins("INNER JOIN categories ON tests.category_id = categories.id")
       .where(categories: {title: category_title})
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
 
-  has_many :test_users, dependent: :destroy
+  has_many :tests
+  has_many :test_users
   has_many :tests, through: :test_users, dependent: :destroy
 
   def levels(lvl)
-    Test.joins(:test_users)
+    Test.(:test_users)
           .where(test_users: {user_id: self.id},
                  tests: {level: lvl})
   end
