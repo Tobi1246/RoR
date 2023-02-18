784dfe55a480509e84fc7de33618180289223c38
Tobi1246 <d.morox@ya.ru>
2023-02-19 01:19:04 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,8 +6,8 @@ class Test < ApplicationRecord
   has_many :users, through: :test_users, dependent: :destroy
   has_many :questions, dependent: :destroy
 
-  validates :level, numericality: { onle_integer: true, greater_than_or_equal_to: 0 }, presence: true
-  validate :validate_min_level, :validate_max_level
+  validates :level, numericality: { onle_integer: true, greater_than_or_equal_to: 0,
+                                                        less_than_or_equal_to: 3 }, presence: true
 
   scope :easy_level, -> { where(level: 1) }
   scope :midle_level, -> { where(level: 2) }
@@ -18,15 +18,5 @@ class Test < ApplicationRecord
   def self.desc_ttitle_and_category(category_title)
     joins(:category)
       .where(categories: { title: category_title })
-  end
-
-  private
-
-  def validate_min_level
-    errors.add(:level) if level.to_i < 1
-  end
-
-  def validate_max_level
-    errors.add(:level) if level.to_i > 3
   end
 end
