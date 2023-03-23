b28f8b1318906c76105eb50c356c0183bbe65e81
Tobi1246 <d.morox@ya.ru>
2023-03-23 16:26:59 +0300

    TestPassage

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,12 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show start]
+  before_action :find_user, only: %i[start]
+
   def index
     @tests = Test.all
   end
 
-  def show
-    @test = Test.find(params[:id])
-  end
+  def show; end
 
   def new
     @test = Test.new
@@ -20,7 +21,20 @@ class TestsController < ApplicationController
     end
   end
 
+  def start
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
+
   private
+
+  def find_user
+    @user = User.first
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
 
   def tests_params
     params.require(:test).permit(:title, :level, :category_id, :author_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,16 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: "Test",
                            foreign_key: "author_id",
                            dependent: :destroy              
-  has_many :test_users
-  has_many :tests, through: :test_users, dependent: :destroy
+  has_many :test_passages
+  has_many :tests, through: :test_passages, dependent: :destroy
 
   validates :firstname, presence: true
 
   def levels(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
