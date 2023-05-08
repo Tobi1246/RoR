914386a35a976b50ff10fcfd7ac0e3383f57351e
Tobi1246 <d.morox@ya.ru>
2023-05-08 15:54:26 +0300

    fix

--- app/javascript/utilities/password_input.js
+++ app/javascript/utilities/password_input.js
@@ -1,20 +1,26 @@
 document.addEventListener('turbo:load', function() {
+
   const form = document.getElementById('new_user')
-  const password = form.user_password
-  const confirmPassword = form.user_password_confirmation
-  const passwordCheck = document.getElementById('password-check')
+  const confirmPassword = document.getElementById('user_password_confirmation')
 
-confirmPassword.addEventListener('input', function() {
-  if (confirmPassword.value === '') {
-    passwordCheck.innerHTML = '';
-    return;
-  }
+  if (form && confirmPassword) {
 
-  if (password.value === confirmPassword.value) {
-    passwordCheck.innerHTML = '<div class="alert alert-success">passwords match</div>'
-  } else {
-    passwordCheck.innerHTML = `<div class="alert alert-danger">passwords don't match</div>`
+    const password = form.user_password
+    const passwordCheck = document.getElementById('password-check')
+  
+    confirmPassword.addEventListener('input', function() {
+      if (confirmPassword.value === '') {
+        passwordCheck.innerHTML = '';
+        return;
+      }
+
+      if (password.value === confirmPassword.value) {
+        passwordCheck.innerHTML = '<div class="alert alert-success">passwords match</div>'
+      } else {
+        passwordCheck.innerHTML = `<div class="alert alert-danger">passwords don't match</div>`
+      }
+
+    })
   }
-})
 })
 
