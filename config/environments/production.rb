c48fb4d1a2f17165632621027d3dafcffee79794
Tobi1246 <d.morox@ya.ru>
2023-05-20 13:33:34 +0300

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testguru_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'tobi-testguru.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'http://tobi-testguru.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    adress:              'smtp.gmail.com',
+    domain:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
