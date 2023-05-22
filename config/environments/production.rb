0861179c57988f0a4673f622b2ae07265dde874d
Tobi1246 <d.morox@ya.ru>
2023-05-23 00:36:18 +0300

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.mailer_sender = 'mail@testguru.com'
-
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: 'tobi-testguru', port: 5000 }
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -71,19 +66,24 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "testguru_production"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'tobi-testguru.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'http://tobi-testguru.herokuapp.com'
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    adress:              'smtp.gmail.com',
-    domain:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true 
-  }
+    config.mailer_sender = 'mail@testguru.com'
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'tobi-testguru.herokuapp.com', port: 5000 }
+
+ # config.action_mailer.perform_caching = false
+ # config.action_mailer.default_url_options = { host: 'tobi-testguru.herokuapp.com' }
+ # Rails.application.routes.default_url_options[:host] = 'http://tobi-testguru.herokuapp.com'
+ # config.action_mailer.delivery_method = :smtp
+ # config.action_mailer.smtp_settings = {
+ #   adress:              'smtp.gmail.com',
+ #   domain:              'smtp.gmail.com',
+ #   port:                 587,
+ #   user_name:            ENV['SMTP_USERNAME'],
+ #   password:             ENV['SMTP_PASSWORD'],
+ #   authentication:       'plain',
+ #   enable_starttls_auto: true 
+ # }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
