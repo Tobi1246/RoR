d2daa0d8382a5ea78cc5ce94f6f4e41049846382
Tobi1246 <d.morox@ya.ru>
2023-05-23 02:06:25 +0300

    test

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -3,6 +3,6 @@ class FeedbackMailer < ApplicationMailer
     @feedback = feedback
     @admin = User.find_by(type: 'Admin')
 
-    mail to: @admin.email,from: @feedback.user.email, subject: 'Feedback'
+    mail(to: @admin.email,from: @feedback.user.email, subject: 'Feedback')
   end  
 end
