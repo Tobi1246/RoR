a0e12c1809b48f933dd65d36376328ce4b070446
Tobi1246 <d.morox@ya.ru>
2023-05-23 20:12:16 +0300

    translate+fix

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ Rails.application.routes.draw do
   end
 
   resources :test_passages, only: %i[show update] do
-    resources :gists, only: :create
     member do
+      post :gist, to: 'gists#create'
       get :result
     end
   end
