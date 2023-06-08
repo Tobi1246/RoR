43c42b77f1e5da73c6a5d34af130f7bb0bbc38bd
Tobi1246 <d.morox@ya.ru>
2023-06-08 16:16:08 +0300

    ашы

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -6,13 +6,4 @@ class Badge < ApplicationRecord
   validates :img, presence: true
   validates :cunductions, presence: true
 
-  IMG_FOR_PASSAGE_TEST = [ "nill",
-                           "https://mir-s3-cdn-cf.behance.net/user/276/8853769.549b42d8ad386.jpg",
-                           "http://ru.meneger.net/images/photo/1548618680.jpg",
-                           "https://image.emojipng.com/226/5139226.jpg" ].freeze
-
-  def self.img(test_passage)
-     IMG_FOR_PASSAGE_TEST[test_passage.test.level] || 0
-  end
-
 end
